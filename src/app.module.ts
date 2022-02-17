f6cd25e862445cd84a46889bb5be8733c8d0b819
Jacques Twizeyimana <sandbergjacques500@gmail.com>
2022-02-17 16:31:26 +0200

    feat: tokens CRUD resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MetersModule } from './meters/meters.module';
+import { TokensModule } from './tokens/tokens.module';
 
 @Module({
-  imports: [MetersModule],
+  imports: [MetersModule, TokensModule],
   controllers: [AppController],
   providers: [AppService],
 })
