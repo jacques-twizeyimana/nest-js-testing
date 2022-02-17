28d3d587bc17198cfb3c3822e26a502d7069adc2
Jacques Twizeyimana <sandbergjacques500@gmail.com>
2022-02-17 16:17:54 +0200

    feat: meter CRUD resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MetersModule } from './meters/meters.module';
 
 @Module({
-  imports: [],
+  imports: [MetersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/meters/meters.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { MetersService } from './meters.service';
+import { CreateMeterDto } from './dto/create-meter.dto';
+import { UpdateMeterDto } from './dto/update-meter.dto';
+
+@Controller('meters')
+export class MetersController {
+  constructor(private readonly metersService: MetersService) {}
+
+  @Post()
+  create(@Body() createMeterDto: CreateMeterDto) {
+    return this.metersService.create(createMeterDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.metersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.metersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateMeterDto: UpdateMeterDto) {
+    return this.metersService.update(+id, updateMeterDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.metersService.remove(+id);
+  }
+}
