7d785c3f15e8f65e826256a9379441e197a79035
Jacques Twizeyimana <sandbergjacques500@gmail.com>
2022-02-17 16:31:57 +0200

    feat: users CRUD res

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MetersModule } from './meters/meters.module';
 import { TokensModule } from './tokens/tokens.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MetersModule, TokensModule],
+  imports: [MetersModule, TokensModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
